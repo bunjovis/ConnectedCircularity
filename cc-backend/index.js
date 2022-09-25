59b65c96a8a86ccfb7b3d9d67da2e178c8e5b119
OpaJaa <71966364+OpaJaa@users.noreply.github.com>
2022-09-25 20:21:11 +0300

    Add skeleton-backend

---
+++ cc-backend/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.get("/", (req, res) => {
+  res.json({ message: "Connected Circularity Backend up and running!" });
+});
+app.listen(port, () => {
+  console.log("Server listening at http://localhost:${port}");
+});

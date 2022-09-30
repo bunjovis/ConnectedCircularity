798aa6b126d418d6f72d6adea269dd9e14e3a704
OpaJaa <71966364+OpaJaa@users.noreply.github.com>
2022-09-30 14:41:28 +0300

    Add ts-support, nodemon, modify gitignore

--- cc-backend/index.js
+++ cc-backend/index.js
@@ -1,15 +1,20 @@
-const express = require("express");
-const app = express();
-const port = 3000;
-app.use(express.json());
-app.use(
-  express.urlencoded({
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+const port = process.env.PORT;
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({
     extended: true,
-  })
-);
+}));
 app.get("/", (req, res) => {
-  res.json({ message: "Connected Circularity Backend up and running!" });
+    res.send("Connected Circularity Backend with TypeScript up and running!");
 });
 app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
+    console.log(` [server]: Server listening at http://localhost:${port}`);
 });

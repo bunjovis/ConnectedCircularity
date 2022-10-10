a2ded8d5d3f0f05f59a3b4dbcddface5d5557e7f
rvsape <saara.pekkala@tuni.fi>
2022-10-10 11:41:53 +0300

    initial cc-db-service api design

--- cc-db-service/src/app.ts
+++ cc-db-service/src/app.ts
@@ -1,5 +1,9 @@
 import express, { Request, Response } from 'express';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import * as OpenApiValidator from 'express-openapi-validator';
 
 dotenv.config();
 
@@ -9,9 +13,33 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
-app.get('/', (request: Request, response: Response) => {
+// serve swagger documentation at endpoint http://localhost:4001/api-docs/
+const options = {
+  customCss: '.swagger-ui .topbar { display: none }',
+};
+app.use(
+  '/api-docs',
+  swaggerUi.serve,
+  swaggerUi.setup(YAML.load('./openapi.yaml'), options)
+);
+
+//validator middleware
+app.use(
+  OpenApiValidator.middleware({
+    apiSpec: './openapi.yaml',
+    validateRequests: true,
+    validateResponses: false,
+  })
+);
+
+app.get('/', (_: Request, response: Response) => {
   response.send('CC Backend service root');
+});
+
+app.get('/v1/system/ping', (_: Request, response: Response) => {
+  response.send('pong');
 });
 
 app.listen(PORT, () => {

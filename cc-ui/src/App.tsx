56677e1b3481eb4523a1523e36ca3b5fa5000e43
olivena <olivia.takkinen@gmail.com>
2022-10-11 18:03:33 +0300

    deleted chakraUI from app.tsx

--- cc-ui/src/App.tsx
+++ cc-ui/src/App.tsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import './App.css'
-import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
   return (
-    <ChakraProvider>
-      <div className="App">
-        <h1>Welcome to Connected Circularity!</h1>
-      </div>
-    </ChakraProvider>
+    <div className="App">
+      <h1>Welcome to Connected Circularity!</h1>
+    </div>
   )
 }
 

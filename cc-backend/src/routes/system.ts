624cb570540e34cf5164b822055c1fbb165fcac4
OpaJaa <71966364+OpaJaa@users.noreply.github.com>
2022-10-21 23:07:12 +0300

    Minor fixes

--- cc-backend/src/routes/system.ts
+++ cc-backend/src/routes/system.ts
@@ -4,6 +4,10 @@ import { Error, ItemInfo, Item} from '../types';
 
 const sysRouter = Router();
 
+sysRouter.get('/', (_: Request, response: Response) => {
+  response.send("Connected Circularity Backend with TypeScript up and running!");
+});
+
 sysRouter.get('/v1/system/ping', (_: Request, response: Response) => {
     response.send('pong');
   });

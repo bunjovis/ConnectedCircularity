eefa29dd0348c028d370362c925d9ec92ab47e8d
olivena <olivia.takkinen@gmail.com>
2022-10-24 12:30:20 +0300

    cc folder stucture and files added and organized

--- cc-ui/README.md
+++ cc-ui/README.md
@@ -9,3 +9,9 @@ Vite + React + TypeScript, user interface of the system
 1. Run `npm install` on command line
 2. Run `npm run dev` on command line to run the program.
 3. Open Google Chrome browser and open the user interface by going to the given localhost address
+
+## Folders/Files explaned
+Login - login to Connected Circularity
+Home - odottavat (waiting) and käsitellyt (processed)
+TransferForm - "Tee ilmoitus Materiaalitoriin" (new ad to materiaalitori)
+MTLogin - Pop-up window to login to Materiaalitori from CC
--- cc-ui/src/App.tsx
+++ cc-ui/src/App.tsx
@@ -4,12 +4,16 @@ import { Routes, Route, Link } from "react-router-dom"
 //import Header  from './components/header/Header'
 
 import Login from './components/pages/Login/Login'
+import Home from './components/pages/Home/Home'
+import TransferForm from './components/pages/TransferForm/TransferForm'
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path='/' element={<Login/>} />
+        <Route path='/Home' element={<Home/>} />
+        <Route path='/New Item' element={<TransferForm/>} />
       </Routes>
   </div>
   );

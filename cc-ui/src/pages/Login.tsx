1db797d2f6c551093e39d2dc6946c35eed79f9cd
olivena <olivia.takkinen@gmail.com>
2022-10-26 19:05:33 +0300

    stylechanges in login

--- cc-ui/src/pages/Login.tsx
+++ cc-ui/src/pages/Login.tsx
@@ -1,16 +1,24 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
+import {useState} from 'react'
+import { Route, Router } from 'react-router-dom'
 
-//import app from './../cc-pk-service/src/app'
+//import app from '../../cc-pk-service/src'
 
 import { 
     Heading,
-    Container,
+    Stack,
+    Link,
     ButtonGroup, 
     Select,
     chakra,
     Input,
-    Button
+    Button,
+    ChakraProvider,
+    extendTheme,
+    Flex,
+    TagLeftIcon,
+    useControllableProp, 
+    useControllableState
 } from "@chakra-ui/react"
 import "@fontsource/montserrat"; 
 
@@ -20,31 +28,71 @@ const FormControl = chakra("form", {
  
 const FormLabel = chakra("text", {
     baseStyle: {
+        float: 'left',
         borderRadius: 'base',
-        color: 'red'
+        color: '#EE0004'
     }
 })
 
-const Login = () => {
+function Login() {
+    type State = {
+        username: string,
+        password: string,
+        message: string,
+        token: string
+      };
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+    
+        console.log(event.currentTarget.elements);
+        console.log(event.currentTarget.elements[0]);
+        //token = app.getToken(username, password);
+
+    };
+
     return (
-        <Container>
-            <Heading>Kirjaudu palveluun</Heading>
-            <FormControl>
-                <FormLabel>Palveluntarjoaja</FormLabel>
-                <Select borderColor='red' >
-                    <option>Palveluntarjoaja</option>
-                    <option>2.</option>
-                </Select>
+        <Flex
+            flexDirection="column"
+            //TO DO: width and height to full page
+            width='100wh'
+            height='100vh'
+            backgroundColor="#E5E5E5"
+            justifyContent="center"
+            alignItems="center" 
+            >
+            <form
+                method="post"
+                name="login_credentials"
+                onSubmit={handleSubmit}>
+                <Stack
+                    spacing={3}
+                    p="3rem"
+                    backgroundColor="#F6F6F6"
+                    boxShadow="md">
+                    
+                    <Heading>Kirjaudu palveluun</Heading>
+                
+                    <FormLabel>Palveluntarjoaja</FormLabel>
+                    <Select borderColor='#EE0004' >
+                        <option>Palveluntarjoaja</option>
+                    </Select>
 
-                <FormLabel>Käyttäjänimi</FormLabel>
-                    <Input borderColor='red' type='text'/>
+                    <FormLabel>Käyttäjänimi</FormLabel>
+                    <Input name='username' borderColor='#EE0004' type='text'/>
 
-                <FormLabel>Salasana</FormLabel>
-                    <Input borderColor='red' type={'password'} />
-
-            </FormControl>
-            <Button colorScheme='blue'>Kirjaudu</Button>
-        </Container>
+                    <FormLabel>Salasana</FormLabel>
+                    <Input name='password' borderColor='#EE0004' type={'password'} />
+                    <Button 
+                        type="submit"
+                        textTransform='uppercase'
+                        colorScheme='blue'
+                        >
+                            Kirjaudu
+                    </Button>
+                </Stack>
+            </form>
+        </Flex>
     )
 }
 

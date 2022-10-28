d0b72f4996f25ec7ee67676f29df9f5653098f7f
Hussain Ali <ali.1.hussain@nokia.com>
2022-10-28 06:46:20 +0300

    Add Rules for Eslint

--- cc-backend/.eslintrc.js
+++ cc-backend/.eslintrc.js
@@ -1,24 +1,24 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "overrides": [
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-        "semi": ["error", "always"],
+  "env": {
+      "browser": true,
+      "es2021": true
+  },
+  "extends": [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended"
+  ],
+  "overrides": [
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+      "ecmaVersion": "latest",
+      "sourceType": "module"
+  },
+  "plugins": [
+      "@typescript-eslint"
+  ],
+  rules: {
+    "semi": ["error", "always"],
     "quotes": "off",
     "no-dupe-else-if": ["error"],
     "eqeqeq": ["error", "always"],
@@ -42,5 +42,5 @@ module.exports = {
     "max-lines-per-function": "warn",
     "prefer-object-spread": "warn",
     "prefer-template": "warn"
-    }
+  }
 }

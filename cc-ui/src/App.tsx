5d158882b19b8bde1e4c85187ac990878e2cd4ac
olivena <olivia.takkinen@gmail.com>
2022-11-02 15:58:52 +0200

    Did changes  as commented

--- cc-ui/src/App.tsx
+++ cc-ui/src/App.tsx
@@ -2,15 +2,18 @@ import React from 'react'
 import {Route, Routes} from 'react-router-dom'
 import './App.css'
 
+import Header from './components/header/Header'
 import Login from './pages/Login'
 import Home from './pages/Home'
 import TransferForm from './pages/TransferForm'
 
 function App() {
+
   return (
     <div className="App">
+      <Header></Header>
       <Routes>
-        <Route path='/' element={<Login/>} />
+        <Route path='*' element={<Login/>} />
         <Route path='/home' element={<Home/>} />
         <Route path='/new_item' element={<TransferForm/>} />
       </Routes>

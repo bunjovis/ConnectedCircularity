6d79b519114d34c341dd823460dd04f7c54916f5
olivena <olivia.takkinen@gmail.com>
2022-11-03 08:36:00 +0200

    cleaned the code

--- cc-ui/src/pages/Login.tsx
+++ cc-ui/src/pages/Login.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import {useState} from 'react'
-import {Route, Routes, useNavigate} from 'react-router-dom'
+import {useNavigate} from 'react-router-dom'
 import axios from 'axios';
-
-import Home from './Home'
 
 import { 
     Heading,

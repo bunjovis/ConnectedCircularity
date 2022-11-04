aca32c459ac31ec11bd60be05f7e3248e9bbcdb8
OpaJaa <71966364+OpaJaa@users.noreply.github.com>
2022-11-04 12:36:27 +0200

    Add type to iteminfo

--- cc-backend/src/routes/itemRoutes.ts
+++ cc-backend/src/routes/itemRoutes.ts
@@ -13,13 +13,10 @@ const itemRouter = Router();
 itemRouter.get(
   '/v1/items/:userId',
   async (request: Request, response: Response, next: NextFunction) => {
-    //request.header('Authorization');
-    //console.log(request.headers.authorization);
     try {
       let wholeToken: any = request.headers.authorization ?? '';
       let token = getToken(wholeToken);
       const userId: string = request.params.userId;
-      console.log('Token: ' + token);
 
       const itemsPK = await getItemsPK(token, userId);
       //const itemsDB = await getItemsDB(request.params.userId);
@@ -42,7 +39,7 @@ itemRouter.get(
       let wholeToken: any = request.headers.authorization ?? '';
       let token = getToken(wholeToken);
       const itemId: string = request.params.itemId;
-      const item = await getItemInfo(token, itemId);
+      const item: ItemInfo = await getItemInfo(token, itemId);
       response.json(item);
     } catch (error: any) {
       response.json({

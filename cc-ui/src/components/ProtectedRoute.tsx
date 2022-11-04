a57e8e75a9272091742210080043c68e53cf79c2
rvsape <saara.pekkala@tuni.fi>
2022-11-04 20:29:21 +0200

    mid dev

--- cc-ui/src/components/ProtectedRoute.tsx
+++ cc-ui/src/components/ProtectedRoute.tsx
@@ -1,7 +1,12 @@
+import React, { ReactElement } from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthProvider';
 
-export const ProtectedRoute = ({ children }) => {
+interface Props {
+  children: ReactElement;
+}
+
+export const ProtectedRoute: React.FC<Props> = ({ children }) => {
   const { user } = useAuth();
   console.log(user);
 

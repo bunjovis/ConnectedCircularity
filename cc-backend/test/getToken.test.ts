a163bc68101a75ccdf6519754a3fc2fea5b22b68
OpaJaa <71966364+OpaJaa@users.noreply.github.com>
2022-11-06 16:24:33 +0200

    Change test description, remove unnecessary imports, restore root package '

--- cc-backend/test/getToken.test.ts
+++ cc-backend/test/getToken.test.ts
@@ -1,11 +1,8 @@
 import { expect } from 'chai';
-//var chai = require("chai");
-//var app = require("../dist/src/index.js")
-//var getToken = require("../dist/src/utils.js");
 import { getToken } from '../src/utils';
 
 describe('GetToken', () => {
-  it('Splits the token and returns the tail', () => {
+  it('Splits the token, checks Bearer and returns the tail', () => {
     expect(getToken('Bearer xxxyyyzzz')).to.equal('xxxyyyzzz');
     expect(getToken('NotABearer xxxyyyzzz')).to.equal('Not a bearer token!');
   });

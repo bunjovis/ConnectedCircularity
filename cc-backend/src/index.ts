2616b82dd7aa08e6d3fbcf11b632e68768e8a3f8
OpaJaa <71966364+OpaJaa@users.noreply.github.com>
2022-11-06 15:00:41 +0200

    Feature/cc backend get token (#26)
    * Fix openapi item schema, generate new api.ts
    
    * Add first GET -drafts
    
    * Item info
    
    * Fix requests
    
    * Add draft of POST user
    
    * Remove some unused stuff
    
    * Move first GET over validator, not really needed so not in openapi.yaml for now
    
    * Add bearer auth to item -related GETs, not globally at this point
    
    * Add more drafts for paths
    
    * Modify post config
    
    * Implement paths
    
    * Fix eslint warnings/errors
    
    * Comments
    
    * Add test version for getting items from PK
    
    * Some tweaks
    
    * Fix error situation with items
    
    * Refactor code, not ready
    
    * Refactor folders
    
    * Add pk-stuff to env.example. Used while ui not ready
    
    * Minor fixes
    
    * Change route files
    
    * Remove pk-service from repo before merge request
    
    * Add pk-service -stuff back, gonna have some conflict-time later...
    
    * Format code
    
    * Add token as request header
    
    * Modify README
    
    * Re-add removed .env.example
    
    * Update iteminfo object
    
    * Remove dotenv config
    
    * Remove dotenv config
    
    * add dotenv to app.ts
    
    * Temporary fix for eslint, will update when actual rules are ready
    
    * Add get tokens from headers and redirect
    
    * Fix requests, still not working, commit only for backupping
    
    * Still trying to get it to work. No idea why it is not working
    
    * Remove obsolete code
    
    * Fix npm-paths, token still not working
    
    * Still not working, token is not transferred to pk-service
    
    * Change back Bearer to Token
    
    * Token now working in both routes, tested with modified pk-service
    
    * Add getToken function for token splitting and checking
    
    * Add type to iteminfo
    
    * Add itemInfo type
    
    * Change returns in iteminfo
    
    * Update header from token to authorization
    
    * Some minor changes
    
    * Update iteminfo in openapi.yaml
    
    * fix eslint errors
    
    * Fix some eslint warnings
    
    * Add new package-lock after huge conflicts
    
    * Change ItemInfo to match pk-service
    
    * Fix eslint errors, a lot of warnings still exist
    
    * Add Metadata to match pk-service
    
    * Fix pk .env.example, add chai to package
    
    Co-authored-by: hannakortetjarvi <kortetjarvihanna@gmail.com>
    Co-authored-by: hannakortetjarvi <59920267+hannakortetjarvi@users.noreply.github.com>

--- cc-backend/src/index.ts
+++ cc-backend/src/index.ts
@@ -1,18 +1,16 @@
-import express, { Express, Request, Response, NextFunction } from "express";
-import swaggerUi from "swagger-ui-express";
-import YAML from "yamljs";
-import dotenv from "dotenv";
-import axios from "axios";
-import * as OpenApiValidator from "express-openapi-validator";
-import sysRouter from "./routes/systemRoutes";
-import itemRouter from "./routes/itemRoutes";
-import advertRouter from "./routes/advertRoutes";
-import userRouter from "./routes/userRoutes";
+import express, { Express, Request, Response, NextFunction } from 'express';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import * as OpenApiValidator from 'express-openapi-validator';
+import bodyParser from 'body-parser';
+import sysRouter from './routes/systemRoutes';
+import itemRouter from './routes/itemRoutes';
+import advertRouter from './routes/advertRoutes';
+import userRouter from './routes/userRoutes';
 
-import { Error } from "./types";
-import { Item } from "./types";
-import { UserInfo } from "./types";
-import { Config } from "./types";
+import { Error } from './types';
 
 dotenv.config();
 
@@ -20,41 +18,43 @@ const app: Express = express();
 const port = process.env.PORT;
 
 app.use(express.json());
+app.use(bodyParser.json());
 app.use(
   express.urlencoded({
-    extended: true,
+    extended: true
   })
 );
+app.use(cors());
+
+app.listen(port, () => {
+  console.log(` [server]: Server listening at http://localhost:${port}`);
+});
 
 // serve swagger documentation at endpoint http://localhost:3000/api-docs/
 const options = {
-  customCss: ".swagger-ui .topbar { display: none }",
+  customCss: '.swagger-ui .topbar { display: none }'
 };
 
 app.use(
-  "/api-docs",
+  '/api-docs',
   swaggerUi.serve,
-  swaggerUi.setup(YAML.load("./openapi.yaml"), options)
+  swaggerUi.setup(YAML.load('./openapi.yaml'), options)
 );
 
 //validator middleware
 app.use(
   OpenApiValidator.middleware({
-    apiSpec: "./openapi.yaml",
+    apiSpec: './openapi.yaml',
     validateRequests: true,
-    validateResponses: false,
+    validateResponses: false
   })
 );
 
-app.use((err: Error, _: Request, res: Response, __: NextFunction) => {
+app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
   // format error
   res.status(err.code || 500).json({
-    message: err.message,
+    message: err.message
   });
-});
-
-app.listen(port, () => {
-  console.log(` [server]: Server listening at http://localhost:${port}`);
 });
 
 app.use(itemRouter, advertRouter, userRouter, sysRouter);

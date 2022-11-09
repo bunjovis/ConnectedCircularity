1fd1c8fcb770a84e2a650660cf2481732df912df
Minja Survonen <minja.survonen@tuni.fi>
2022-11-09 12:35:45 +0200

    Changing route to  read header

--- cc-mt-service/src/routes/mtServiceRoutes.ts
+++ cc-mt-service/src/routes/mtServiceRoutes.ts
@@ -7,9 +7,12 @@ const mtRouter = Router();
 
 mtRouter.post('/advert',async (req:Request, res:Response) => {
     const data:AdvertData = req.body;
-    // should include attributes type and data but data is undefined
-    const response = await postAdvert(data).catch(e => console.log(e));
-    if (response) res.send(response);
+    const token = req.header.toString ?? '';
+    console.log(token);
+    const resp = token;
+    // const response = await postAdvert(data).catch(e => console.log(e));
+    // if (response) res.send(response);
+    if (resp) res.send(resp);
     else {
         const err:Error = {
             message:"Cannot add advert",

92c4c00d7b14f107af6db574f9c25cceee0b1358
Minja Survonen <minja.survonen@tuni.fi>
2022-11-27 17:24:39 +0200

    Fix workflow errors

--- cc-backend/src/routes/advertRoutes.ts
+++ cc-backend/src/routes/advertRoutes.ts
@@ -9,7 +9,7 @@ advertRouter.post(
   async (request: Request, response: Response) => {
     try {
       const advert: AdvertData = request.body;
-      const wholeToken: any = request.headers.authorization ?? '';
+      const wholeToken = request.headers.authorization ?? '';
       const token = getToken(wholeToken);
       if (token === 'Not a bearer token!') {
         console.log('Invalid token');
@@ -25,7 +25,7 @@ advertRouter.post(
         response.status(postToMT.status);
         response.json(postToMT.data);
       }
-    } catch (error: any) {
+    } catch (error) {
       if (!error.response) {
         console.log('Unexpected error: 500');
         response.status(500);
